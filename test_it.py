580a47e3135726ce8a8a62adfdfa464520e1ef25
Yehor Smoliakov <yehors@ukr.net>
2022-11-27 20:50:41 +0200

    Add test script

---
+++ test_it.py
@@ -0,0 +1,22 @@
+from ukro_g2p.predict import G2P
+
+g2p = G2P('ukro-base-uncased')
+
+sentences = [
+    'я хочу спитати',
+    'Звича́йно на то́му яко́го подаро́вано',
+]
+
+for s in sentences:
+    print(s)
+
+    words = s.lower().split(' ')
+
+    words_phonemes = []
+    for word in words:
+        word_no_accent = [it for it in word if it != '́']
+        phonemes = g2p(word_no_accent)
+
+        words_phonemes.append(''.join(phonemes))
+    
+    print(' '.join(words_phonemes))
